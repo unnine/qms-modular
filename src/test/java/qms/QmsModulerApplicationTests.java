d952ab571830d99b853a27b65e9939c43c212126
unnine <eonninej@gmail.com>
2025-03-27 08:08:06 +0900

    create project

---
+++ src/test/java/qms/QmsModulerApplicationTests.java
@@ -0,0 +1,13 @@
+package qms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsModulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
